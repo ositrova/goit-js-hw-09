a8a5f442a82ddf133f79c8d5419e0c21edbf02a2
ositrova <110326755+ositrova@users.noreply.github.com>
2023-01-06 17:21:28 +0200

    task2 and yask3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,12 +1,46 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+  const submitBtn = document.querySelector('button');
+  const delayInput = document.querySelector('[name="delay"]');
+  const stepInput = document.querySelector('[name="step"]');
+  const amountInput = document.querySelector('[name="amount"]');
+
+const formData = {
+  delay: 0,
+  step: 0,
+  amount: 0,
+}
+delayInput.addEventListener('input', takingInputInfo);
+stepInput.addEventListener('input', takingInputInfo);
+amountInput.addEventListener('input', takingInputInfo);
+
+const takingInputInfo = (e) => { formData[e.target.name] = Number(e.target.value); }
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(Notify.success(`Fulfilled promise ${position} in ${delay}ms`)) 
+      } else {
+        reject(Notify.failure(`Fulfilled promise ${position} in ${delay}ms`)) 
+      }
+    }, delay )
+  })
 }
 
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+
+  for (let i = 1; i <= formData.amount; i += 1) {
+    createPromise(i, formData.delay)
+    .then(value => {})
+    .catch(error => {}); 
+  
+    formData.delay += formData.step;
+  }  
+})
 
 
 
